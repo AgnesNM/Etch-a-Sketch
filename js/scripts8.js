32c15931bb88857444be6d65428cd661e64e00ac
Agnes NM <agnesndutamwaniki@gmail.com>
2019-06-10 09:11:55 +0300

    create any number of divs depending on entered value in the prompt

--- js/scripts8.js
+++ js/scripts8.js
@@ -34,7 +34,7 @@ function someDivs2(){
 //creating 64 divs
 //spread them out into an equal number of rows and columns
 
-//background color on hover, haven't figured the pixelated part out though
+//background color on hover, haven't figured the pixelated part out though (used border for now)
 
 let squareDivs = document.getElementsByClassName("squares");
 for(let squareDiv of squareDivs){
@@ -43,19 +43,39 @@ for(let squareDiv of squareDivs){
 
 function handler(event){
     if (event.type == 'mouseover') {                        
-        event.target.style.backgroundColor = "plum";          
+        event.target.style.backgroundColor = "plum";  
+        event.target.style.border = "dotted";        
     }
 }
              
      
-//reset button
-function resetB(){
+//reset button to clear 
+function resetB(){    
     let resetButton = document.getElementById("reset-btn");
-    resetButton.addEventListener('click', function(event){
-        document.getElementById("grid-container").innerHTML= "";
-    });
+    resetButton.addEventListener('click', function(event){        
+        document.getElementById("grid-container").innerHTML= ""; 
+        anyDivs();        
+    }); 
 }
 
 resetB();
 
+//create any number of divs entered in the prompt
+
+function anyDivs(n){
+    n = prompt("How many squares should each side of the new grid have?");        
+    for (i=0;i<n;i++){
+        let div1 = document.createElement("div");
+        div1.className = "squares";
+        div1.innerHTML = 1;                                    
+    
+        for(x=1;x<n;x++){
+            let rows = document.createElement("div");
+            rows.className = "squares";  
+            rows.innerHTML = 1;
+            document.getElementById("grid-container").appendChild(rows);       
+        } 
+    document.getElementById("grid-container").appendChild(div1);
+    }
+} 
 
