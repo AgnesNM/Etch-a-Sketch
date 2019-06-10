c7a93fa42236286da500c82a2fad616654cc5635
Agnes NM <agnesndutamwaniki@gmail.com>
2019-06-10 10:14:02 +0300

    change the background of the new grid after reset

--- js/scripts9.js
+++ js/scripts9.js
@@ -36,26 +36,26 @@ function someDivs2(){
     }
 }
 
-//creating an initial 16X16 grid
-//creating 64 divs
-//spread them out into an equal number of rows and columns
-
 //background color on hover, haven't figured the pixelated part out though (used border for now)
 
-let squareDivs = document.getElementsByClassName("squares");
-for(let squareDiv of squareDivs){
-    squareDiv.onmouseover = handler;
-}
-
-function handler(event){
-    if (event.type == 'mouseover') {                        
-        event.target.style.backgroundColor = "plum";  
-        event.target.style.border = "dotted";        
+function bground(){
+    let squareDivs = document.getElementsByClassName("squares");
+    for(let squareDiv of squareDivs){
+        squareDiv.onmouseover = handler;
+    }
+    
+    function handler(event){
+        if (event.type == 'mouseover') {                        
+            event.target.style.backgroundColor = "plum";  
+            event.target.style.border = "dotted";        
+        }
     }
 }
-             
-     
-//reset button to clear 
+
+bground();
+
+
+//creating an initial 16X16 grid //reset button to clear 
 function resetB(){    
     let resetButton = document.getElementById("reset-btn");
     resetButton.addEventListener('click', function(event){        
@@ -76,10 +76,10 @@ function anyDivs(n){
         div1.innerHTML = 1;                                    
     
         for(x=1;x<n;x++){
-            let rows = document.createElement("div");
-            rows.className = "squares";  
-            rows.innerHTML = 1;
-            document.getElementById("grid-container").appendChild(rows);       
+            let div1 = document.createElement("div");
+            div1.className = "squares";  
+            div1.innerHTML = 1;
+            document.getElementById("grid-container").appendChild(div1);       
         } 
     document.getElementById("grid-container").appendChild(div1);
     }    
@@ -89,7 +89,9 @@ function anyDivs(n){
     let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
     document.documentElement.style.setProperty("--colNum", n);
     n = rowNum;
-    n = colNum;      
+    n = colNum; 
+    let squareDivs = document.getElementsByClassName("squares");  
+    bground();  
 } 
 
 //how to make the divs have a dynamic number of rows and columns
